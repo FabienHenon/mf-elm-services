7b4ed9bad74927e3f3077d9754aa3ace7b7ab268
Fabien Henon <henon.fabien@softcreations.fr>
2022-09-02 17:24:22 +0200

    Add makeId function

--- src/index.js
+++ src/index.js
@@ -197,3 +197,16 @@ export const withEditDefaultNotificationConfig = (config) => ({
     ),
   },
 });
+
+export const makeId = (length) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
